d18bcbad9c09d9d5ce01da2cae458ad3d71674de
Reuven1203 <reuven1203@gmail.com>
2021-12-15 18:30:08 -0500

    .

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,12 +2,18 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navBarLinks = document.getElementsByClassName('navLinks')[0];
 const navTag = document.getElementsByTagName('nav')[0];
 const content = document.getElementsByTagName('body')[0];
-
+const bClicked = false;
 toggleButton.addEventListener('click', () => {
     navBarLinks.classList.toggle('active');
     navTag.classList.toggle('active');
     content.classList.toggle('active');
     toggleButton.classList.toggle('active');
-    console.log(toggleButton);
+    // bclicked = true;
+    // if (bclicked == true){
+    //     navTag.classList.toggle('pull');
+    // }
+  
 })
+
+
 
