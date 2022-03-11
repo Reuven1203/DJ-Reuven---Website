2a7491092e18303abb9d2ec1886cbdb963d6942f
Reuven1203 <reuven1203@gmail.com>
2022-03-10 19:07:27 -0500

    remove gif + gallery animation

--- js/animation.js
+++ js/animation.js
@@ -1,12 +1,18 @@
+var galCard = document.getElementById('gal');
+
 function startAnimation(id) {
     var elmnt = document.getElementById(id);
     elmnt.classList.add("a-animation");
 
 }
-
+// window.pageYOffset-document.body.scrollTop >= 553
 window.addEventListener("scroll", function(){
-    if (window.pageYOffset-document.body.scrollTop >= 553){
+    console.log(galCard.style.top);
+    if (window.pageYOffset-document.body.scrollTop >= 425){
         startAnimation("gal-heading")
         startAnimation("gal")
+        startAnimation("gal-msg");
+        startAnimation("img-box");
     }
 })
+
